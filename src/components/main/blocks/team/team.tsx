80e85b6acc7dec7d0137294d39d3d712762c284b
Riddhi Parekh <parekhriddhi33@gmail.com>
2020-12-16 23:21:50 +0530

    mend

--- src/components/main/blocks/team/team.tsx
+++ src/components/main/blocks/team/team.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Carousel from "react-elastic-carousel"
+// import Carousel from "react-elastic-carousel"
 import styles from "./team.module.scss"
 
 export default ({ block }) => {
