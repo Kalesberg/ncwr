d4fa095249618496bd23eba0e1eb988b92a72482
Riddhi Parekh <parekhriddhi33@gmail.com>
2021-01-04 14:22:30 +0530

    Fix animation in hero-slider

--- src/components/main/blocks/hero/hero.tsx
+++ src/components/main/blocks/hero/hero.tsx
@@ -1,59 +1,61 @@
 import React from "react"
 import styles from "./hero.module.scss"
+import "./slider.scss"
+import Carousel from "react-elastic-carousel"
 
 export default ({ block }) => {
-  const [slideIndex, setSlideIndex] = React.useState(0)
-  const switchSlides = () => {
-    setSlideIndex(block.heroGroup.length - 1 > slideIndex ? slideIndex + 1 : 0)
-  }
-  React.useEffect(() => {
-    if (block.heroGroup.length > 1) setInterval(switchSlides, 5000)
-  }, [])
 
   return (
     <section className={styles.section}>
       <div className={styles.heroSection}>
         <div className={styles.slideshowContainer}>
-          {block.heroGroup.map((slide, index) => {
-            return (
-              <div
-                className={`${styles.mySlides} fade animate__animated ${
-                  block.heroGroup.length > 1 && "animate__fadeInLeft"
-                }`}
-                key={index}
-                style={{
-                  display: slideIndex === index ? "flex" : "none",
-                  backgroundImage: `url('${slide.image}')`,
-                }}
-              >
-                <div className={styles.heroSectionLeft}>
-                  <h2>{slide.heading}</h2>
-                  <p>{slide.text}</p>
+          <Carousel
+            itemsToShow={1}
+            showArrows={false}
+            pagination={block.heroGroup.length > 1}
+            renderPagination={({ pages, activePage, onClick }: any) => {
+              return (
+                <div className={styles.heroSectionBottom}>
+                  {pages.map((page) => (
+                    <div
+                      key={page}
+                      className={`${styles.heroBoxRight} ${styles.moveBox1} ${
+                        activePage === page ? styles.active : ""
+                      }`}
+                      onClick={() => onClick(page)}
+                    >
+                      <p>0{page}</p>
+                      <span>
+                        [News/announcement headline {page} , TBD]
+                      </span>
+                    </div>
+                  ))}
                 </div>
-                {block.heroGroup.length > 1 && (
-                  <div className={styles.heroSectionBottom}>
-                    {block.heroGroup.map((slideDot, dotIndex) => (
-                      <div
-                        key={dotIndex}
-                        className={`${styles.heroBoxRight} ${styles.moveBox1} ${
-                          dotIndex === slideIndex ? styles.active : ""
-                        }`}
-                        onClick={() => setSlideIndex(dotIndex)}
-                      >
-                        <p>0{dotIndex + 1}</p>
-                        <span>
-                          [News/announcement headline {dotIndex + 1} , TBD]
-                        </span>
-                      </div>
-                    ))}
+              )
+            }}
+          >
+            {block.heroGroup.map((slide, index) => {
+              return (
+                <div
+                  className={`${styles.mySlides} fade animate__animated ${
+                    block.heroGroup.length > 1 && "animate__fadeInLeft"
+                  }`}
+                  key={index}
+                  style={{
+                    backgroundImage: `url('${slide.image}')`,
+                  }}
+                >
+                  <div className={styles.heroSectionLeft}>
+                    <h2>{slide.heading}</h2>
+                    <p>{slide.text}</p>
                   </div>
-                )}
-                <div className={styles.bottomIcon}>
-                  <img className={styles.icon} src="/down.svg" />
+                  <div className={styles.bottomIcon}>
+                    <img className={styles.icon} src="/down.svg" />
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+            </Carousel>
         </div>
       </div>
     </section>
